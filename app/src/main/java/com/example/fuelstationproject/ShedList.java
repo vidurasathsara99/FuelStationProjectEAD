90379576858e7d1f45cee70e6d148e4310355747
DeshaniMAWD <it19015736@my.sliit.lk>
2022-10-17 17:30:44 +0530

    Added petrol sheds near you page

---
+++ app/src/main/java/com/example/fuelstationproject/ShedList.java
@@ -0,0 +1,14 @@
+package com.example.fuelstationproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShedList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shed_list);
+    }
+}
