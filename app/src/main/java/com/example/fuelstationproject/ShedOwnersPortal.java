b08c014eaad73b170df98eb632b54ed666cb29e3
DeshaniMAWD <it19015736@my.sliit.lk>
2022-10-17 17:05:32 +0530

    Added shed owner's portal

---
+++ app/src/main/java/com/example/fuelstationproject/ShedOwnersPortal.java
@@ -0,0 +1,14 @@
+package com.example.fuelstationproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShedOwnersPortal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shed_owners_portal);
+    }
+}
